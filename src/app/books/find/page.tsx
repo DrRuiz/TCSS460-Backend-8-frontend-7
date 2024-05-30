c06113edfe831231ba410be595094bcf2b15197a
DRuiz <93886686+DrRuiz@users.noreply.github.com>
2024-05-29 23:46:34 -0700

    Pagination completely working for get all

--- src/app/books/find/page.tsx
+++ src/app/books/find/page.tsx
@@ -9,31 +9,6 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import { Divider, FormControl, FormHelperText, InputLabel, List, MenuItem, Pagination, Select, SelectChangeEvent, Stack, TextField } from "@mui/material";
 
-// interface IRatings {
-//   average: number;
-//   count: number;
-//   rating_1: number;
-//   rating_2: number;
-//   rating_3: number;
-//   rating_4: number;
-//   rating_5: number;
-// }
-
-// interface IUrlIcon {
-//   large: string;
-//   small: string;
-// }
-
-// interface IBook {
-//   isbn13: number;
-//   authors: string;
-//   publication: number;
-//   original_title: string;
-//   title: string;
-//   ratings: IRatings;
-//   icons: IUrlIcon;
-// }
-
 interface IBook {
   isbn13: number;
   authors: string;
@@ -56,7 +31,6 @@ function BookListItem(
   return (
     <ListItem onClick={() => onGetDetails({book})}>
         <ListItemAvatar>
-            {/* <Avatar src= {book.icons.large}"https://images.gr-assets.com/books/1447303603m/2767052.jpg"  */}
             <Avatar src= {book.image_small_url}
             variant="square" 
             sx={{width: "98px", height: "146px", marginRight: "1em", border: "3px solid", borderColor: "secondary.main"}}/>
@@ -64,8 +38,6 @@ function BookListItem(
         <ListItemText
         primaryTypographyProps={{fontSize: '24px',}}
         secondaryTypographyProps={{fontSize: '20px'}}
-        // primary={"Title: " + /*`${book.title}`*/"The Hunger Games"}
-        // secondary={"By " + /*`${book.author}`*/"Suzanne Collins"}
         primary={"Title: " + `${book.title}`}
         secondary={"By " + `${book.authors}`}
         />
@@ -76,20 +48,21 @@ export default function Find() {
 
   const [books, setBooks] = React.useState<IBook[]>([]);
   const [currentPage, setCurrentPage] = React.useState(1);
-  const [booksPerPage, setBooksPerPage] = React.useState(10);
+  const [booksPerPage, setBooksPerPage] = React.useState(20);
+  const [lastPage, setLastPage] = React.useState(10);
 
-  // const perPage = 10;
 
     //Use a look here to get all the data from all the books.
   React.useEffect(() => {
-    fetch('http://localhost:4000/books/all2?page=${curentPage}&limit=${booksPerPage}', {
+    fetch(`http://localhost:4000/books/all2?page=${currentPage}&limit=${booksPerPage}`, {
       method: "GET",
     })
       .then((res) => res.json())
       .then((data) => {
         setBooks(data.books);
+        setLastPage(data.pagination.totalPages);
       });
-  }, []);
+  }, [currentPage, booksPerPage]);
 
   //Loads the details page
   const handleDetails = (
@@ -103,35 +76,12 @@ export default function Find() {
 
   //Handles the change of the page we are in.
   const handleCurrentPageChange = (event: React.ChangeEvent<unknown>, value: number) => {
-    // setCurrentPage(value);
-    console.dir(value);
-    fetch('http://localhost:4000/books/all2?page=${value}&limit=${booksPerPage}', {
-      method: "GET",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setBooks(data.books);
-      });
+    setCurrentPage(value);
   };
 
   //Handles the change of books per page.
-  // const handleBooksPerPageChange = (event: React.ChangeEvent<unknown>, value: number) => {
-  //   setBooksPerPage(value);
-  //   //Will the other ones then get called as well like the update of the books?
-  // };
-
   const handleBooksPerPageChange = (event: SelectChangeEvent) => {
-    // setBooksPerPage(parseInt(event.target.value, 10));
-
-    // console.dir(event);
-
-    fetch('http://localhost:4000/books/all2?page=${curentPage}&limit=${event.target.value}', {
-      method: "GET",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setBooks(data.books);
-      });
+    setBooksPerPage(parseInt(event.target.value));
   };
 
   return (
@@ -168,12 +118,14 @@ export default function Find() {
                   <MenuItem value={10}>10</MenuItem>
                   <MenuItem value={20}>20</MenuItem>
                   <MenuItem value={30}>30</MenuItem>
+                  <MenuItem value={50}>50</MenuItem>
+                  <MenuItem value={100}>100</MenuItem>
                </Select>
               <FormHelperText>Books Per Page</FormHelperText>
               </FormControl>
               <FormControl sx={{mt:2.5,}}>
                 <Pagination 
-                  count={20} 
+                  count={lastPage} 
                   page={currentPage}
                   onChange={handleCurrentPageChange}
                   color="primary" />

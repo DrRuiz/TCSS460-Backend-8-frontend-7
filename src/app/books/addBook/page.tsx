d078ffc56289f9f5ca126331f0b04a281cf24548
roleniss <ryan.d.anderson@comcast.net>
2024-06-02 12:18:08 -0700

    setup database correctly

--- src/app/books/addBook/page.tsx
+++ src/app/books/addBook/page.tsx
@@ -63,52 +63,52 @@ function BookListItem({book}: {book: IBook}) {
       <Box component="img" src="https://images.gr-assets.com/books/1474154022m/3.jpg" 
       sx={{border: "3px solid", borderColor: "secondary.main", float: "left", marginRight: "1em"}} 
       />
-      {/* <Typography sx={{fontSize: '24px'}}>
-      {book.title}
-      </Typography> */}
       <Typography sx={{fontSize: '24px'}}>
-      Harry Potter and the Sorcerer's Stone (Harry Potter #1)
+      {book.title}
       </Typography>
-      {/* <Typography sx={{fontSize: '12px', color: "text.secondary"}}>
+      {/* <Typography sx={{fontSize: '24px'}}>
+      Harry Potter and the Sorcerer's Stone (Harry Potter #1)
+      </Typography> */}
+      <Typography sx={{fontSize: '12px', color: "text.secondary"}}>
       ISBN: {book.isbn13}
       <br />
       <br />
-      </Typography> */}
-      <Typography sx={{fontSize: '12px', color: "text.secondary"}}>
+      </Typography>
+      {/* <Typography sx={{fontSize: '12px', color: "text.secondary"}}>
       ISBN: 919241982418
       <br />
       <br />
-      </Typography>
-      {/* <Typography sx={{fontSize: '18px'}}>
-      By {book.authors} - Published {book.publication}
-      <br />
       </Typography> */}
       <Typography sx={{fontSize: '18px'}}>
-      By J.K. Rowling - Published 2001
+      By {book.authors} - Published {book.publication_year}
       <br />
       </Typography>
-      {/* <Typography sx={{fontSize: '14px'}}>
-      Total Ratings: {book.ratings.count} / Average Rating: {book.ratings.average}
+      {/* <Typography sx={{fontSize: '18px'}}>
+      By J.K. Rowling - Published 2001
+      <br />
       </Typography> */}
       <Typography sx={{fontSize: '14px'}}>
-      Total Ratings: 1213831 / Average Rating: 4.54
+      Total Ratings: {book.rating_count} / Average Rating: {book.rating_avg}
       </Typography>
       {/* <Typography sx={{fontSize: '14px'}}>
+      Total Ratings: 1213831 / Average Rating: 4.54
+      </Typography> */}
+      <Typography sx={{fontSize: '14px'}}>
         <Rating 
         name='book-rating'
         readOnly
-        value={book.ratings.average}
+        value={book.rating_avg}
         precision={0.5}
         />
-      </Typography> */}
-      <Typography sx={{fontSize: '14px'}}>
+      </Typography>
+      {/* <Typography sx={{fontSize: '14px'}}>
         <Rating 
         name='book-rating'
         readOnly
         value={4.54}
         precision={0.5}
         />
-      </Typography>
+      </Typography> */}
     </Container>
   );
 }
